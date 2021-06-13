a606ad726fc4cc3510cf8603d82a76e4a9f70691
Sibusiso <sibusiso87rn@gmail.com>
2021-06-13 07:18:27 +0300

    Added login screen

---
+++ lib/constants/colors/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const button_color      = const Color.fromRGBO(223, 238, 234, 1);
+  static const button_color_dark = const Color.fromRGBO(24, 95, 86, 1);
+  static const bold_text_color   = const Color.fromRGBO(47, 93, 98, 1);
+  static const value_prop_color  = const Color.fromRGBO(93, 143, 136, 1);
+
+}
